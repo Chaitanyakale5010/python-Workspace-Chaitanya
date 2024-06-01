f3f39b2450b69227cea16db7f4afee0e94ce42ce
Chaitanyakale5010 <152631436+Chaitanyakale5010@users.noreply.github.com>
2024-06-01 22:03:31 +0530

    listdata.py

---
+++ listdata.py
@@ -0,0 +1,9 @@
+#List Data type -> Mutable data type 
+list1 = str
+print(list1)
+List2 = [1,2,4,5,6,'chaitanya']
+print(List2)
+# List through constructor 
+l = list({2,3,5,7})
+print(l)
+print(type(l))
